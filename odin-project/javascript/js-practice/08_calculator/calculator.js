83ee9c70235d62c8f1cdfd9f5c3a7bec0c395d94
oswaldo32s <oswaldo32gonzalez@gmail.com>
2024-05-21 18:07:25 -0600

    Add excercise 08

--- odin-project/javascript/js-practice/08_calculator/calculator.js
+++ odin-project/javascript/js-practice/08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	if (Number.isInteger(a) && Number.isInteger(b)) {
+    return a + b
+  } 
+  return 0
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	if (Number.isInteger(a) && Number.isInteger(b)) {
+    return a - b
+  }
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length > 0) {
+    return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+  }
+	return 0
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if (array.length > 0) {
+    return array.reduce((accumulator, currentValue) => accumulator * currentValue, 1)
+  }
+	return 0
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1
+	for (let i = 1; i <= a; i++) {
+    total *= i
+  }
+
+  return total
 };
 
 // Do not edit below this line
@@ -31,3 +47,5 @@ module.exports = {
   power,
   factorial
 };
+
+console.log(add([]))
