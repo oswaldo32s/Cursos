38f6fcf7ac792690b20a1a1f053b6541118abe14
oswaldo32s <oswaldo32gonzalez@gmail.com>
2024-06-29 10:29:35 -0600

    feat: Add functionality to from's button

--- odin-project/full-stack/javascript/object-contructor/mylibrary-project/script.js
+++ odin-project/full-stack/javascript/object-contructor/mylibrary-project/script.js
@@ -10,3 +10,21 @@ function Book(name, year, author, pages, description, read) {
 }
 
 addBookBtn = document.querySelector('.add-book-btn')
+
+addBookBtn.addEventListener('click', addNewBook)
+
+function addNewBook() {
+    const bookName = document.querySelector('#book-name').value
+    const bookAuthor = document.querySelector('#book-author').value
+    const bookYear = document.querySelector('#book-year').value
+    const bookPages = document.querySelector('#book-pages').value
+    const bookRead = document.querySelector('#book-read').value
+    const bookDescription = document.querySelector('#book-description').value
+
+    const newBook = new Book(bookName, bookAuthor, bookYear, bookPages, bookRead, bookDescription)
+
+    myLibrary.push(newBook)
+
+    const inputs = document.querySelectorAll('.form-input')
+    inputs.forEach(element => element.value = '')
+}
