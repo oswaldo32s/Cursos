4d87f5326c06e418f386ede4d27e8742a5fb4349
oswaldo <oswaldo32gonzalez@gmail.com>
2024-08-10 15:33:17 -0600

    Add total practice with webpack

--- odin-project/full-stack/javascript/webpack-demo/webpack.config.js
+++ odin-project/full-stack/javascript/webpack-demo/webpack.config.js
@@ -1,9 +1,10 @@
+const { type } = require('os');
 const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
