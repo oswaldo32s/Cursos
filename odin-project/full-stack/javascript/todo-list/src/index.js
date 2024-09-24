0426cb08fa44c6e5506d55fb3505f23817d04a47
oswaldo <oswaldo32gonzalez@gmail.com>
2024-09-23 18:32:03 -0600

    Add basic stying to website

--- odin-project/full-stack/javascript/todo-list/src/index.js
+++ odin-project/full-stack/javascript/todo-list/src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 
 function component() {
-  const element = document.createElement('div');
+  const element = document.createElement('h1');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = 'Hello! This is the basic structure for my To do List'
