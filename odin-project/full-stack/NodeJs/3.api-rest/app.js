c521e4c20d963bb8d6b4de6de239ae1f016c4d98
oswaldo <oswaldo32gonzalez@gmail.com>
2024-10-25 22:03:44 -0600

    node practice

---
+++ odin-project/full-stack/NodeJs/3.api-rest/app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const crypto = require("crypto");
+const movies = require("./movies.json");
+const { validateMovie, validatePartialMovie } = require("./schemas/movies");
+
+const app = express();
+
+app.disable("x-powered-by");
+
+// Middleware
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "hola mundo" });
+});
+
+app.get("/movies", (req, res) => {
+  const { genre } = req.query;
+
+  if (genre) {
+    const filteredMovies = movies.filter((movie) =>
+      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
+    );
+    return res.json(filteredMovies);
+  }
+  res.json(movies);
+});
+
+app.get("/movies/:id", (req, res) => {
+  const { id } = req.params;
+
+  const movie = movies.find((item) => item.id === id);
+  if (movie) return res.json(movie);
+  res.status(404).json({ message: "Movie not found" });
+});
+
+app.post("/movies", (req, res) => {
+  const result = validateMovie(req.body);
+
+  if (result.error) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
+  }
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data,
+  };
+
+  movies.push(newMovie);
+
+  res.status(201).json(newMovie);
+});
+
+app.patch("/movies/:id", (req, res) => {
+  const { id } = req.params;
+  const movieIndex = movies.findIndex((movie) => movie.id === id);
+
+  if (movieIndex === -1) {
+    res.status(404).json({ message: "Movie not found" });
+  }
+
+  const result = validatePartialMovie(req.body);
+
+  if (result.error) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data,
+  };
+
+  movies[movieIndex] = updateMovie;
+
+  return res.json(updateMovie);
+});
+
+app.listen(3000, () => {
+  console.log(`running on http://localhost:3000`);
+});
